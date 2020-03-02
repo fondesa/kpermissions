966f9d1b3aeda4dd435c10e4f14d82762caa5a57
antoniolig <giorgio.antonioli.dev@gmail.com>
2020-03-02 01:10:21 +0100

    Refactor dependencies

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Fondesa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Deps {
+  const val androidPlugin = "com.android.tools.build:gradle:3.5.0"
+  const val androidxAppCompat = "androidx.appcompat:appcompat:1.1.0"
+  const val androidxFragment = "androidx.fragment:fragment:1.1.0"
+  const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
+  const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
+  const val junit = "junit:junit:4.12"
+  const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61"
+  const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61"
+  const val kPermissions = "com.github.fondesa:kpermissions:2.0.2"
+  const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.0-beta-3"
+  const val mockitoCore = "org.mockito:mockito-core:2.28.2"
+  const val mockitoInLine = "org.mockito:mockito-inline:2.28.2"
+  const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
+  const val robolectric = "org.robolectric:robolectric:4.3"
+}

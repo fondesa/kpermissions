afd4b45c875e707e65b7f64948df7f0300c0c202
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-03-06 22:15:58 +0100

    Upgraded androidx.fragment:fragment from 1.1.0 to 1.2.2 (#60)

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -17,7 +17,7 @@
 object Deps {
   const val androidPlugin = "com.android.tools.build:gradle:3.5.0"
   const val androidxAppCompat = "androidx.appcompat:appcompat:1.1.0"
-  const val androidxFragment = "androidx.fragment:fragment:1.1.0"
+  const val androidxFragment = "androidx.fragment:fragment:1.2.2"
   const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
   const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:0.9.18"
   const val junit = "junit:junit:4.13"

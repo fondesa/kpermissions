843563d4f66bd738e482a536e2ab312c5d88731f
antoniolig <giorgio.antonioli.dev@gmail.com>
2020-03-20 21:45:34 +0100

    Add RxJava 3 artifact

---
+++ kpermissions-rx3/src/test/kotlin/com/fondesa/kpermissions/rx3/RxExtensionsKtTest.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2020 Fondesa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fondesa.kpermissions.rx3
+
+import android.Manifest
+import com.fondesa.kpermissions.PermissionStatus
+import com.fondesa.kpermissions.request.PermissionRequest
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
+
+/**
+ * Tests for RxExtensions.kt file.
+ */
+class RxExtensionsKtTest {
+    private val request = mock<PermissionRequest>()
+    private val listenerCaptor = argumentCaptor<PermissionRequest.Listener>()
+
+    @Test
+    fun `When request listener is notified, the observer is notified too`() {
+        val observer = request.observe().test()
+
+        verify(request).addListener(listenerCaptor.capture())
+
+        val listener = listenerCaptor.lastValue
+        listener.onPermissionsResult(
+            listOf(
+                PermissionStatus.Granted(Manifest.permission.SEND_SMS),
+                PermissionStatus.Denied.Permanently(Manifest.permission.CALL_PHONE)
+            )
+        )
+
+        observer.assertValueCount(1)
+        observer.assertValues(
+            listOf(
+                PermissionStatus.Granted(Manifest.permission.SEND_SMS),
+                PermissionStatus.Denied.Permanently(Manifest.permission.CALL_PHONE)
+            )
+        )
+
+        listener.onPermissionsResult(
+            listOf(
+                PermissionStatus.Granted(Manifest.permission.SEND_SMS),
+                PermissionStatus.Granted(Manifest.permission.CALL_PHONE)
+            )
+        )
+
+        observer.assertValueCount(2)
+        observer.assertValues(
+            listOf(
+                PermissionStatus.Granted(Manifest.permission.SEND_SMS),
+                PermissionStatus.Denied.Permanently(Manifest.permission.CALL_PHONE)
+            ),
+            listOf(
+                PermissionStatus.Granted(Manifest.permission.SEND_SMS),
+                PermissionStatus.Granted(Manifest.permission.CALL_PHONE)
+            )
+        )
+    }
+
+    @Test
+    fun `When request listener is notified before the observer has been added, the observer does not receive the item on subscription`() {
+        val observable = request.observe()
+
+        verify(request).addListener(listenerCaptor.capture())
+
+        val listener = listenerCaptor.lastValue
+        listener.onPermissionsResult(
+            listOf(
+                PermissionStatus.Granted(Manifest.permission.SEND_SMS),
+                PermissionStatus.Denied.Permanently(Manifest.permission.CALL_PHONE)
+            )
+        )
+
+        val observer = observable.test()
+        observer.assertEmpty()
+    }
+
+    @Test
+    fun `When observer is disposed, the request listener is removed`() {
+        val observer = request.observe().test()
+
+        verify(request).addListener(listenerCaptor.capture())
+
+        observer.dispose()
+
+        verify(request).removeListener(listenerCaptor.lastValue)
+    }
+}

ecfb02bb6db7e044aca16408053e8b29634be435
antoniolig <giorgio.antonioli.dev@gmail.com>
2020-05-16 13:41:39 +0200

    Apply ktlint in buildSrc

--- buildSrc/src/main/kotlin/com/fondesa/kpermissions/buildtools/ApplyFrom.kt
+++ buildSrc/src/main/kotlin/com/fondesa/kpermissions/buildtools/ApplyFrom.kt
@@ -17,23 +17,12 @@
 package com.fondesa.kpermissions.buildtools
 
 import org.gradle.api.Project
-import java.io.File
-import java.util.Properties
 
 /**
- * Reads the properties file with the given name in one of the two paths with this precedence:
- * 1. the root of the module
- * 2. the root of the root project
+ * Applies a script defined in [scriptRootProjectPath].
  *
- * @param fileName the name of the file.
- * @return the [Properties] defined in the given file.
+ * @param scriptRootProjectPath the path relative to the root project in which the script is defined.
  */
-internal fun Project.readPropertiesOf(fileName: String): Properties {
-    val rootPropsFile = rootProject.file(fileName)
-    val propsFile = file(fileName)
-    return readPropertiesFiles(rootPropsFile, propsFile)
-}
-
-private fun readPropertiesFiles(vararg files: File): Properties = Properties().apply {
-    files.filter { file -> file.exists() }.forEach { file -> load(file.inputStream()) }
+internal fun Project.applyFrom(scriptRootProjectPath: String) {
+    apply(mapOf("from" to rootProject.file(scriptRootProjectPath)))
 }

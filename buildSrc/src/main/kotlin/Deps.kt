8fa83da248a06f73bcb35a179fe532a4116b4ccb
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-09-08 20:45:11 +0200

    Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.3.72 to 1.4.0

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -27,7 +27,7 @@ object Deps {
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val junit = "junit:junit:4.13"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.0"
-    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.4"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"

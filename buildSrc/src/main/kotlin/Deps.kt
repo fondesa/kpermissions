f5b198dc468222670d07f9f11b31c77194d46594
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2020-11-16 19:55:45 +0100

    Upgraded com.squareup.leakcanary:leakcanary-android from 2.4 to 2.5

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -29,7 +29,7 @@ object Deps {
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.10"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
     const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.1"
-    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.4"
+    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.5"
     const val mockitoCore = "org.mockito:mockito-core:3.3.3"
     const val mockitoInLine = "org.mockito:mockito-inline:3.3.3"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"

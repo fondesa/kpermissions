b65f62712288359fe4b95309a1b2556841f229a6
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-06-17 23:10:52 +0200

    Update Mockito kotlin to 3.2.0

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -33,7 +33,7 @@ object Deps {
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.7"
     const val mockitoCore = "org.mockito:mockito-core:3.11.1"
     const val mockitoInLine = "org.mockito:mockito-inline:3.11.1"
-    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
+    const val mockitoKotlin = "org.mockito.kotlin:mockito-kotlin:3.2.0"
     const val releasesHubPlugin = "com.dipien:releases-hub-gradle-plugin:2.0.2"
     const val robolectric = "org.robolectric:robolectric:4.5.1"
     const val rxJava2 = "io.reactivex.rxjava2:rxjava:2.2.21"

78e4f74544d204e3df93ab2cf84ec971ac7d34c7
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-07-07 21:37:41 +0200

    Upgraded org.jetbrains.dokka:dokka-gradle-plugin from 1.4.32 to 1.5.0 (#254)

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -23,7 +23,7 @@ object Deps {
     const val androidxJUnit = "androidx.test.ext:junit:1.1.3"
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0"
     const val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0"
-    const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32"
+    const val dokkaPlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.5.0"
     const val gitHubReleasePlugin = "com.github.breadmoirai:github-release:2.2.12"
     const val gradleMavenPublishPlugin = "com.vanniktech:gradle-maven-publish-plugin:0.17.0"
     const val junit = "junit:junit:4.13.2"

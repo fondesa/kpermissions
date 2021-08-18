97758f17c217390d137b984b3a5544a65550279b
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-08-18 16:52:58 +0200

    Upgraded com.android.tools.build:gradle-api from 4.2.2 to 7.0.0

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -16,7 +16,7 @@
 
 object Deps {
     const val androidPlugin = "com.android.tools.build:gradle:7.0.0"
-    const val androidPluginApi = "com.android.tools.build:gradle-api:4.2.2"
+    const val androidPluginApi = "com.android.tools.build:gradle-api:7.0.0"
     const val androidxAppCompat = "androidx.appcompat:appcompat:1.3.0"
     const val androidxFragment = "androidx.fragment:fragment:1.3.5"
     const val androidxFragmentTesting = "androidx.fragment:fragment-testing:1.3.5"

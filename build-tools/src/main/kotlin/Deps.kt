2682a3131696a9065a2ed47b41cf73ac53f7d3c9
Giorgio Antonioli <giorgio.antonioli.dev@gmail.com>
2021-12-17 22:21:26 +0100

    Upgraded androidx.appcompat:appcompat from 1.3.1 to 1.4.0 (#278)

--- build-tools/src/main/kotlin/Deps.kt
+++ build-tools/src/main/kotlin/Deps.kt
@@ -17,7 +17,7 @@
 object Deps {
     const val androidPlugin = "com.android.tools.build:gradle:7.0.2"
     const val androidPluginApi = "com.android.tools.build:gradle-api:7.0.2"
-    const val androidxAppCompat = "androidx.appcompat:appcompat:1.3.1"
+    const val androidxAppCompat = "androidx.appcompat:appcompat:1.4.0"
     const val androidxFragment = "androidx.fragment:fragment:1.3.6"
     const val androidxFragmentTesting = "androidx.fragment:fragment-testing:1.3.6"
     const val androidxJUnit = "androidx.test.ext:junit:1.1.3"
